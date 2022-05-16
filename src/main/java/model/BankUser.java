9a2cca2863ba64545f9a8355b4b6ab22fa178bb3
SzymonW66 <szymekwajs@gmail.com>
2022-05-16 23:07:33 +0200

    Changes by HARDPC

--- src/main/java/model/BankUser.java
+++ src/main/java/model/BankUser.java
@@ -4,10 +4,10 @@ public class BankUser {
     private String firstName;
     private String secondName;
     private String pesel;
-    private final String login;
-    private final String password;
-    private final double money;
-    private final long accountNumber;
+    private String login;
+    private String password;
+    private double money;
+    private long accountNumber;
 
     public BankUser(String firstName, String secondName, String pesel, String login, String password, double money, long accountNumber) {
         this.firstName = firstName;
@@ -41,6 +41,38 @@ public class BankUser {
 
     public void setPesel(String pesel) {
         this.pesel = pesel;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    public long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(long accountNumber) {
+        this.accountNumber = accountNumber;
     }
 
     @Override
